bfe4ab83ed84e6ce39da662d4cb63695526d4fb1
DATAGNI K. Armand Noël <datagni.k.armand.n@gmail.com>
2024-09-22 18:08:02 +0000

    init

---
+++ app/src/main/java/com/example/showcase_soccer_matches_search_kotlin_compose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.showcase_soccer_matches_search_kotlin_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.showcase_soccer_matches_search_kotlin_compose.ui.theme.ShowcaseSoccerMatchesSearchKotlinComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShowcaseSoccerMatchesSearchKotlinComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShowcaseSoccerMatchesSearchKotlinComposeTheme {
+        Greeting("Android")
+    }
+}
