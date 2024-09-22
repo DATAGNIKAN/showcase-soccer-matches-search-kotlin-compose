f771998f617d659b1da4f0f4d1c822e82f9a8b9b
DATAGNI K. Armand Noël <datagni.k.armand.n@gmail.com>
2024-09-22 22:32:48 +0000

    fix: unused var warning

--- app/src/main/java/com/example/showcase_soccer_matches_search_kotlin_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/showcase_soccer_matches_search_kotlin_compose/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.showcase_soccer_matches_search_kotlin_compose.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
