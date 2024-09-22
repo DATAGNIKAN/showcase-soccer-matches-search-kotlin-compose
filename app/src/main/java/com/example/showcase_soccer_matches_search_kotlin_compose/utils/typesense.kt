d8682056cdfc3e838618b1d4816c2452d375ae0e
DATAGNI K. Armand Noël <datagni.k.armand.n@gmail.com>
2024-09-22 20:09:58 +0000

    chore: typesense client setup

---
+++ app/src/main/java/com/example/showcase_soccer_matches_search_kotlin_compose/utils/typesense.kt
@@ -0,0 +1,25 @@
+package com.example.showcase_soccer_matches_search_kotlin_compose.utils
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import org.typesense.api.Client
+import org.typesense.api.Configuration
+import org.typesense.resources.Node
+import java.time.Duration
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun setupTypesenseClient(): Client {
+    val nodes: MutableList<Node> = ArrayList()
+    nodes.add(
+        Node(
+            "http",
+            "192.168.10.164", // replace with your wifi IPV4
+            "8108"
+        )
+    )
+
+    val config = Configuration(nodes, Duration.ofSeconds(2), "xyz")
+
+    return Client(config)
+}
